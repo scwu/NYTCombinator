ac08a72079d5f879c60f1833d929d6859ab10fd7
Clara Wu <s.clara.wu@gmail.com>
2012-12-16 03:50:52 -0500

    deleting unnecessary files

--- public/javascripts/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
- $('.content').hide();
-  $('.read').click (function(event) {
-    event.preventDefault();
-    var id = $(this).attr('id')
-    $('.content').filter('#' + id).toggle(function() {
-    });
-  });
-});
